dcde4a04baa8003c7a30be8f877b4cd8fa09510a
cyboranf <cyboranf@gmail.com>
2023-05-30 16:06:16 +0200

    init

---
+++ src/main/java/com/example/drivingschoolmanagement/DrivingSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivingschoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drivingschoolmanagement/DrivingSchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivingschoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
